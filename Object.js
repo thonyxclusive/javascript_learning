ca1ec5fad2a7366bc4e28357f341db574abafecb
thonyxclusive <anthonyobi99@gmail.com>
2023-09-08 19:19:29 +0100

    updated object practice

--- Object.js
+++ Object.js
@@ -5,8 +5,8 @@
 }
 var firstChild = person.children[0];
 console.log(firstChild)*/
-
-var savingsAccount = {
+//------------------------------------------------------------------------------------------------------
+/*var savingsAccount = {
     balance: 1000,
     interestRatePercent: 1,
     deposit: function addMoney(amount) {
@@ -26,4 +26,30 @@ var savingsAccount = {
     }
 };
 
-console.log(savingsAccount.printAccountSummary());
+console.log(savingsAccount.printAccountSummary()); */
+//----------------------------------------------------------------------------------------------------
+var facebookProfile = { 
+    name: "Obi Anthony",
+    friends : 1527,
+    messages : ["Hey, i'm new here", "Todays a beautiful day", "I think i just became a millionaire"],
+    
+   postMessage: function(message){
+           facebookProfile.messages.push("I am hungry");
+   },
+   deleteMessage: function(index){
+           // In the following splice() method call,
+           // - argument 1 = index from where the element has to be deleted
+           // - argument 2 = count of elements to be deleted
+           facebookProfile.messages.splice(index, 1);
+       },
+   addFriend: function(){
+           facebookProfile.friends = facebookProfile.friends +=1 ;
+           //friends += 1; // this statement is equivalent to the above
+       },
+   removeFriend: function(){
+           if(facebookProfile.friends>0)
+               facebookProfile.friends = facebookProfile.friends -=1;
+       }
+   };
+
+   console.log(facebookProfile.deleteMessage)
